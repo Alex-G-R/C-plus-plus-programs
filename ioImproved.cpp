11b474d4ca36ffc5225a99c368c128eee5747dce
Alex Grabski <101558615+Alex-G-R@users.noreply.github.com>
2023-11-20 18:28:11 +0100

    io.cpp but user input file

---
+++ ioImproved.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+int main(){
+
+    std::string filename;
+    std::cin >> filename;
+
+    std::ofstream file (filename.c_str(), std::ios::app);
+
+    if(file.is_open()){
+        std::cout << "file opened successfully! \n";
+    } else {
+        std::cout << "That's a hard error here! \n";
+        return -1;
+    }
+
+    std::vector<std::string> names;
+
+    names.push_back("Alex");
+    names.push_back("James");
+    names.push_back("Jimmy");
+    names.push_back("Ton");
+    names.push_back("Jerry");
+    names.push_back("Cherry");
+    names.push_back("Rose");
+    names.push_back("Mia");
+    names.push_back("Chloe");
+
+    for( std::string n : names) {
+        file << n << "\n";
+    }
+
+    file.close();
+    return 0;
+}
